0222ead608616fe0ef53ca46cf1537c84114ba34
V1ny <viny13.99@gmail.com>
2016-09-27 16:54:47 -0300

    Grande atualização

---
+++ pfc/pfc1/termos-politica.php
@@ -0,0 +1,109 @@
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>E-BET</title>
+
+    <link href="dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/flat-ui.css" rel="stylesheet">
+    <link href="docs/assets/css/demo.css" rel="stylesheet">
+    <link href="index.css" rel="stylesheet">
+
+    <script src="dist/js/vendor/html5shiv.js"></script>
+    <script src="dist/js/vendor/respond.min.js"></script>
+
+    <style>
+        body  {
+            background-color: white;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+
+
+
+
+
+
+
+
+    <nav class="navbar navbar-inverse" role="navigation">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+                <span class="sr-only">Toggle navigation</span>
+            </button>
+            <a class="navbar-brand" href="index.php" >E-bet</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse-01">
+            <ul class="nav navbar-nav navbar-left">
+                <li ><a href="pagina admin.php">Admin</a></li>
+                <li class="dropdown">
+                    <a href="jogo.php" class="dropdown-toggle" data-toggle="dropdown">Campeonatos<b class="caret"></b></a>
+                    <span class="dropdown-arrow"></span>
+                    <ul class="dropdown-menu">
+                        <li><a href="jogolista.php">Lista de Jogos</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="cadastro.php">Cadastrar</a></li>
+                <li><a href="calendario.php">Calendário</a></li>
+                <li><a href="informacao.php">Notícias</a></li>
+            </ul>
+            <form class="navbar-form navbar-right" action="#" role="search">
+                <div class="form-group">
+                    <div class="input-group">
+                        <input class="form-control" id="navbarInput-01" type="search" placeholder="Pesquise aqui!">
+                        <span class="input-group-btn">
+            <button type="submit" class="btn"><span class="fui-search"></span></button>
+          </span>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </nav>
+    <h3>Termos de Uso e Política de Privacidade</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis nunc massa. Duis ut ipsum fermentum, egestas tellus et, consectetur quam. Phasellus vehicula consequat odio, vel dapibus est ullamcorper a. Nam vitae cursus metus, vel ornare massa. Nulla nunc risus, lobortis at tristique vitae, feugiat a magna. Vestibulum euismod risus nunc. Curabitur vitae ornare diam, tincidunt ultricies enim.
+
+    Vestibulum sit amet urna molestie, pretium tellus a, ullamcorper lectus. Nunc libero elit, pellentesque id pellentesque ultricies, convallis ac dolor. Ut lacinia orci felis, vitae aliquam elit cursus id. Quisque a ultrices risus. Pellentesque tincidunt ante eu laoreet pretium. Quisque eu metus et felis ornare lobortis. Nullam ornare id turpis eu pretium.
+
+    Ut ut ultrices lacus. Fusce molestie imperdiet leo vitae imperdiet. Nullam non nulla feugiat, iaculis nulla quis, tristique ante. In cursus laoreet tincidunt. Pellentesque eu eleifend justo. Fusce sit amet augue scelerisque, mollis lacus tempor, tincidunt urna. Integer ut est non nibh consequat fringilla in eu lectus. Aenean pretium erat maximus pharetra viverra. In varius imperdiet metus, eu auctor velit sagittis at. Vivamus porta ligula ligula.
+
+    Nunc at nisl ut augue dignissim iaculis. Mauris venenatis ante nulla, vel pellentesque ex pharetra vel. Praesent vitae egestas orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam eleifend nec nisl ut feugiat. Sed ac lobortis nisi, id sodales risus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+    Cras sed nibh augue. Praesent porttitor hendrerit nulla id congue. Integer vitae lobortis nisl, at finibus neque. Vivamus vulputate cursus metus, eu laoreet nibh scelerisque ut. Phasellus quis nulla blandit, volutpat urna eu, faucibus arcu. Phasellus quis enim ut sapien commodo commodo. Nulla fringilla diam at nulla posuere ultrices. Curabitur molestie nunc ac leo pharetra consequat. Donec eget pharetra sem, eu mattis leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet mauris finibus, posuere nunc ac, egestas nibh. Integer augue ligula, porttitor vel facilisis sit amet, fringilla eget turpis. Fusce mattis pellentesque turpis, et tristique metus consectetur vel.
+
+    Phasellus gravida lorem ligula, quis mattis nisi blandit ut. Nullam pellentesque mauris et quam dictum ornare. Nulla fermentum turpis a facilisis cursus. In hac habitasse platea dictumst. Nam a tortor nec ex scelerisque varius. Duis eleifend urna vitae ultrices porta. Fusce nibh ligula, tincidunt ac nisi et, aliquam pretium lorem. Cras sem tellus, rhoncus at vestibulum viverra, vulputate a massa.
+
+    Aliquam a erat sodales, eleifend dui vitae, vehicula nulla. Phasellus id risus quis libero pellentesque congue. Maecenas non libero consectetur, sodales orci in, luctus justo. Aenean tristique pretium neque, vitae pellentesque justo rutrum sit amet. Praesent aliquam arcu nec velit convallis tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras nec maximus ante. Fusce dui mi, congue eget sapien quis, sagittis fringilla dui. Vestibulum quis elit leo. In ut lacus egestas turpis pellentesque tincidunt. Nunc tristique lectus ut dolor hendrerit lobortis. Fusce condimentum urna vel risus sagittis consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam sit amet facilisis ipsum. Proin id erat ornare, sollicitudin odio eget, luctus tellus. Cras sem urna, auctor non velit non, vestibulum varius magna.
+
+    Vivamus convallis ullamcorper risus, at placerat nibh. Nam non lacinia magna, semper mattis dui. Nullam ultrices tellus bibendum felis ultrices porta. Maecenas et felis mauris. Fusce diam ex, cursus eget suscipit sit amet, blandit id velit. Vestibulum pulvinar sed orci ut sollicitudin. Donec ac velit elit. Fusce turpis lacus, sodales et odio non, finibus lacinia quam. Nulla et rhoncus augue, a tincidunt nulla. Suspendisse pellentesque lorem laoreet vehicula dictum. Curabitur ligula quam, volutpat ac ultrices eget, hendrerit at magna.
+
+    Donec at quam imperdiet lacus placerat varius. Etiam convallis ultrices mauris, vel fringilla risus viverra ut. Sed vestibulum ut erat ut consequat. Aliquam est ligula, vestibulum nec erat a, mollis mollis purus. Ut lobortis ligula sit amet bibendum tempus. Nullam risus est, fringilla nec dolor molestie, consequat facilisis ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit tortor quis tincidunt pretium. Donec commodo ligula a enim pellentesque egestas. Fusce ac leo et dolor mattis lobortis.
+
+    Nam eu vestibulum nisl. Etiam a condimentum magna. Maecenas convallis, lacus in pulvinar elementum, turpis nulla consectetur dolor, nec viverra erat quam id elit. Nulla leo metus, dapibus vitae fermentum in, accumsan id urna. Ut sit amet lectus mauris. Donec tempus consequat interdum. Maecenas est diam, ultricies et arcu et, placerat volutpat ante. Pellentesque posuere, ante et facilisis convallis, nibh arcu pulvinar magna, id laoreet metus odio in nibh.
+
+    Praesent fermentum purus pretium odio aliquet, fringilla malesuada libero dapibus. Fusce lorem mauris, maximus non iaculis ut, suscipit tempor ex. Praesent sollicitudin sem elementum, tincidunt erat ac, aliquam ex. Vivamus ut sollicitudin est. Pellentesque malesuada ipsum at enim accumsan ornare. Phasellus neque diam, vehicula sit.</p>
+    </div>
+<footer>
+    <div class="container">
+        <div class="row demo-row">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+                © Todos os Direitos reservados á sua mãe
+            </div>
+            <div class="col-xs-4">
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="dist/js/vendor/jquery.min.js"></script>
+<script src="dist/js/flat-ui.min.js"></script>
+<script src="docs/assets/js/application.js"></script>
+
+<script>
+    videojs.options.flash.swf = "dist/js/vendors/video-js.swf"
+</script>
+</body>
