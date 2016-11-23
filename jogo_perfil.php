4956f064f6f4e01ac4a1af14e44ec8dcbfa045b4
V1ny <viny13.99@gmail.com>
2016-11-23 16:00:46 -0200

    footer

--- jogo_perfil.php
+++ jogo_perfil.php
@@ -68,38 +68,8 @@
     ?>
 </table>
     </div>
-<footer>
-    <div class="container">
-        <div class="row demo-row">
-            <div class="col-xs-5">
-                <h6>Nossos Contatos</h6>
-                <a href="https://www.facebook.com/vinycius13" class="fui-facebook"> Vinycius Silveira</a></br>
-                <a href="https://plus.google.com/116622724535038811811" class="fui-google-plus"> viny13.99</a></br>
-                <a href="https://www.facebook.com/leandro.andrei.cunha?ref=ts&fref=ts" class="fui-facebook"> Leandro Cunha</a></br>
-                <a href="https://plus.google.com/100994567177990828022" class="fui-google-plus"> Leandro</a></br>
-            </div>
-            <div class="col-xs-2"></div>
-            <div class="col-xs-5">
-                <h6>Contatos do IFC-Araquari</h6>
-                <a href="http://araquari.ifc.edu.br/" class="fui-window"> Site do Instituto</a></br>
-                <a href="https://www.facebook.com/ifc.oficial" class="fui-facebook"> Facebook do Instituto</a></br>
-                <span class="fui-home"> <b>(47) 3803-7200</b></span>
-
-            </div>
-
-
-        </div>
-        <div class="row demo-row">
-            <div class="col-xs-4">
-            </div>
-            <div class="col-xs-4">
-                © Todos os Direitos reservados à sua mãe
-            </div>
-            <div class="col-xs-4">
-            </div>
-        </div>
-    </div>
-</footer>
+    <?php require ("footer.php");
+    ?>
     <script src="dist/js/vendor/jquery.min.js"></script>
     <script src="dist/js/flat-ui.min.js"></script>
     <script src="docs/assets/js/application.js"></script>
