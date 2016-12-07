548e1bcc8bb8a4facf69e6069fbffd876790b8ec
frangyt <leandro.frangit@gmail.com>
2016-12-07 18:36:22 -0200

    cor de quem ganhou e perdeu

--- partidas_lista.php
+++ partidas_lista.php
@@ -74,7 +74,7 @@ else{
         while ($id <= $idmax) {
             $sql = "select   idpartidas, max(idtimes) as idtime1, min(idtimes) as idtime2,  idcampeonatos, data, sigla_campeonato
                      from partidas, times_partida, campeonatos, times  where idpartidas = times_partidas_idpartidas
-                       and partidas_idcampeonatos = idcampeonatos and idtimes = times_partidas_idtimes and idpartidas=$id ;
+                       and partidas_idcampeonatos = idcampeonatos and idtimes = times_partidas_idtimes and idpartidas=$idmax ;
                      ";
             $resultado = mysqli_query($conexao, $sql);
             while ($linha = mysqli_fetch_array($resultado)) {
@@ -85,16 +85,40 @@ else{
                   <td><a class='btn btn-block btn-lg btn-inverse' href='partidas_perfil.php?id=" . $linha["idpartidas"] . "'>" . $linha["idpartidas"] . "</a>              </td>
                  <td>" . $linha["data"] . "</td>
              <td><a class='btn btn-block btn-lg btn-inverse' href='campeonato_perfil.php?id=" . $linha["idcampeonatos"] . "'>" . $linha["sigla_campeonato"] . "</a>              </td>";
-
+                    $sql4 = " select nome_time, idtimes from times, times_partida, partidas where idpartidas = $idmax 
+                        and ganhador = 's' and times_partidas_idpartidas = idpartidas and  
+                        times_partidas_idtimes = idtimes";
+                    $resultado4 = mysqli_query($conexao, $sql4);
+                    $linha4 = mysqli_fetch_array($resultado4);
                     $sql2 = "select sigla_times from times where idtimes = $idtime1;";
                     $resultado2 = mysqli_query($conexao, $sql2);
                     $linha2 = mysqli_fetch_array($resultado2);
-                    echo "<td><a class='btn btn-block btn-lg btn-inverse' href='times_perfil.php?id=" . $linha["idtime1"] . "'>" . $linha2["sigla_times"] . "</a    >              </td>
-            <td><a href='partidas_perfil.php?id=" . $id . "'> <span class='fui-cross'></a></span></td>";
+                    ?>
+                    <td><a class='btn btn-block btn-lg btn-<?php
+                    if ($idtime1 != $linha4['idtimes']){
+                        echo "danger";
+                    }
+                    else{
+                    echo "success";
+                    }
+                     ?>
+                     <?php
+                        echo "' href='times_perfil.php?id=" . $linha["idtime1"] . "'>" . $linha2["sigla_times"] . "</a    >              </td>
+            <td><a href='partidas_perfil.php?id=" . $idmax . "'> <span class='fui-cross'></a></span></td>";
                     $sql3 = "select sigla_times from times where idtimes = $idtime2;";
                     $resultado3 = mysqli_query($conexao, $sql3);
                     $linha3 = mysqli_fetch_array($resultado3);
-                    echo "<td><a class='btn btn-block btn-lg btn-inverse' href='times_perfil.php?id=" . $linha["idtime2"] . "'>" . $linha3["sigla_times"] . "</a>              </td>";
+                    ?>
+                    <td><a class='btn btn-block btn-lg btn-<?php
+                    if ($idtime2 != $linha4['idtimes']){
+                        echo "danger";
+                    }
+                    else{
+                    echo "success";
+                    }
+                     ?>
+                     <?php
+                        echo "' href='times_perfil.php?id=" . $linha["idtime2"] . "'>" . $linha3["sigla_times"] . "</a>              </td>";
 
                     $sql5 = "select ganhador from times_partida where times_partidas_idpartidas = $id";
                     $resultado5 = mysqli_query($conexao, $sql5);
@@ -102,12 +126,9 @@ else{
                     if ($linha5["ganhador"] == null) {
 
 
-                        echo "<td><a class='btn btn-block btn-lg btn-primary' href = 'atualiza_resultado.php?id=" . $id . "' > atualizar vencedor </a > </td >";
+                        echo "<td><a class='btn btn-block btn-lg btn-primary' href = 'atualiza_resultado.php?id=" . $idmax . "' > atualizar vencedor </a > </td >";
                     } else {
-                        $sql4 = " select nome_time, idtimes from times, times_partida, partidas where idpartidas = $id and ganhador = 's' and times_partidas_idpartidas = idpartidas and  
-                        times_partidas_idtimes = idtimes";
-                        $resultado4 = mysqli_query($conexao, $sql4);
-                        $linha4 = mysqli_fetch_array($resultado4);
+
                         echo "<td><a class='btn btn-block btn-lg btn-primary' href='times_perfil.php?id=" . $linha4["idtimes"] . "' > " . $linha4["nome_time"] . "</a> </td>";
                     }
 
@@ -115,7 +136,7 @@ else{
                     echo " </tbody>";
                 }
             }
-            $id = $id+1;
+            $idmax = $idmax - 1;
             echo "</br>";
         }
 
